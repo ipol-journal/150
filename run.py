99b659bb9af99753cf8654273c5fda80032959d7
Jyotsnarajan <jyotsnarajan.815@gmail.com>
2023-12-01 14:27:21 +0530

    required src code, updated bm3d outdated link

--- run.py
+++ run.py
@@ -10,13 +10,20 @@ ap = argparse.ArgumentParser()
 ap.add_argument("sigma", type=int)
 args = ap.parse_args()
 
-with open('rmse_IHS.txt', 'w') as file:               
-    p = ['imdiff_ipol', 'input_0.png', 'ihs.png', 'diffInputIHS.png']
-    subprocess.run(p, stdout=file)
-
 #run algo
+def merge_files(input_files, output_file):
+    with open(output_file, 'w') as output:
+        for input_file in input_files:
+            with open(input_file, 'r') as file:
+                # Read the RMSE data from the current input file
+                data_RMSE = float(file.readline().strip())
+                parts = input_file.split('.')[0].split('_')
+                RMSE = parts[1]+' '+parts[0]                
+                # Write the float number to the output file
+                output.write(f"{RMSE.upper()} : {data_RMSE}\n")
+
 # Denoise image
-subprocess.run(['denoise_'+str(args.sigma), '-c', 'input_0.png'])
+subprocess.run(['denoise', '-c', 'input_0.png'])
 
 # Compute image differences
 subprocess.run(['imdiff', 'clean.png', 'nn.png', 'diffnn.png'])
@@ -25,24 +32,27 @@ subprocess.run(['imdiff', 'clean.png', 'ssann.png', 'diffssann.png'])
 
 # Compute image rmse
 with open('rmse_nn.txt', 'w') as stdout:
-    subprocess.run(['imdiff', '-mrmse', 'clean.png', 'nn.png'], stderr=stdout)
+    subprocess.run(['imdiff', '-mrmse', 'clean.png', 'nn.png'], stderr=stdout, stdout=stdout)
 
 with open('rmse_bm3d.txt', 'w') as stdout:
-    subprocess.run(['imdiff', '-mrmse', 'clean.png', 'bm3d.png'], stderr=stdout)
+    subprocess.run(['imdiff', '-mrmse', 'clean.png', 'bm3d.png'], stderr=stdout, stdout=stdout)
 
 with open('rmse_ssann.txt', 'w') as stdout:
-    subprocess.run(['imdiff', '-mrmse', 'clean.png', 'ssann.png'], stderr=stdout)
+    subprocess.run(['imdiff', '-mrmse', 'clean.png', 'ssann.png'], stderr=stdout, stdout=stdout)
+
+# List of input files
+input_files = ['rmse_nn.txt', 'rmse_bm3d.txt', 'rmse_ssann.txt']
+
+# Merge RMSE data into one file
+merge_files(input_files, 'output.txt')
 
 # Resize for visualization (new size of the smallest dimension = 200)
 (sizeX, sizeY) = PIL.Image.open('input_0.png').size
 zoomfactor = max(1, int(math.ceil(200.0/min(sizeX, sizeY))))
 (sizeX, sizeY) = (zoomfactor*sizeX, zoomfactor*sizeY)
 
-for filename in ['input_0_sel', 'noisy', 'mark', 'bm3d', 'clean',
+for filename in ['input_0', 'noisy', 'mark', 'bm3d', 'clean',
     'nn', 'ssann', 'diffnn', 'diffssann', 'diffbm3d']:
     im = PIL.Image.open(filename + '.png')
-    im.resize((sizeX, sizeY), method='nearest')
+    im = im.resize((sizeX, sizeY))
     im.save(filename + '_zoom.png')
-
-
-'cp src/NeuralStruct.cpp.10 src/NeuralStruct.cpp && make denoise -j4 -C src -f makefile && mv src/denoise src/denoise_10'
